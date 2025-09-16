8ac9d7efde029f08fb192c86d2247002704a66ad
Raouf-blip <abdoul-raouf.temirov2@etu.univ-lorraine.fr>
2025-09-16 14:35:56 +0200

    practiendto

---
+++ app/src/application_core/domain/dto/PracticienDTO.php
@@ -0,0 +1,22 @@
+<?
+
+namespace toubilib\core\domain\dto;
+
+class PracticienDTO {
+    public string $id;
+    public string $nom;
+    public string $prenom;
+    public string $ville;
+    public string $email;
+    public string $specialiteid;
+
+    public function __construct(string $id, string $nom, string $prenom, string $ville, string $email, string $specialiteid)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->ville = $ville;
+        $this->email = $email;
+        $this->specialiteid = $specialiteid;
+    }
+}
