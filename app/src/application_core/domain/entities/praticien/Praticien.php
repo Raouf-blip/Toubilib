2a6e244f5c601fe2e78e280fc69a4fb98cd900ba
Raouf-blip <abdoul-raouf.temirov2@etu.univ-lorraine.fr>
2025-09-16 14:24:47 +0200

    Practicien.php

--- app/src/application_core/domain/entities/praticien/Praticien.php
+++ app/src/application_core/domain/entities/praticien/Praticien.php
@@ -5,6 +5,50 @@ namespace toubilib\core\domain\entities\praticien;
 
 class Praticien
 {
- 
+    private string $id;
+    private string $nom;
+    private string $prenom;
+    private string $ville;
+    private string $email;
+    private string $specialiteid;
 
+    public function __construct(string $id, string $nom, string $prenom, string $ville, string $email, string $specialiteid)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->ville = $ville;
+        $this->email = $email;
+        $this->specialiteid = $specialiteid;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function getVille(): string
+    {
+        return $this->ville;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getSpecialiteid(): string
+    {
+        return $this->specialiteid;
+    }
 }
