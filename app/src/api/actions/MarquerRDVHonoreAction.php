8d179f525c31a4e9d348aad21a833f746136ad49
Arman Hayrapetyan <109231389+Arman4498@users.noreply.github.com>
2025-12-01 19:28:13 +0100

    fix bug

--- app/src/api/actions/MarquerRDVHonoreAction.php
+++ app/src/api/actions/MarquerRDVHonoreAction.php
@@ -26,8 +26,16 @@ class MarquerRDVHonoreAction
         try {
             $this->serviceRDV->marquerCommeHonore($rdvId);
         } catch (\Exception $e) {
-            $response->getBody()->write(json_encode(['error' => $e->getMessage()]));
-            return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+            $status = 400;
+            $msg = $e->getMessage();
+            
+            // Gérer les erreurs spécifiques
+            if (strpos($msg, 'inexistant') !== false) {
+                $status = 404;
+            }
+            
+            $response->getBody()->write(json_encode(['error' => $msg], JSON_UNESCAPED_UNICODE));
+            return $response->withStatus($status)->withHeader('Content-Type', 'application/json');
         }
 
         $response->getBody()->write(json_encode(['message' => 'Rendez-vous marqué comme honoré']));
